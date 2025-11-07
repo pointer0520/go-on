406d518a7548cdf92768f11294c6b8a527d8fbef
xzwonroad <xzwonroad@163.com>
2025-11-07 18:04:46 +0800

    Initial commit

---
+++ mysite/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def welcome(request):
+    return HttpResponse("Welcome to Django5!")
+
+urlpatterns = [
+    # urlpatterns里存放的就是项目的全部路由。path的第一个参数是路由地址，第二个参数是对应的视图（view）
+    path('admin/', admin.site.urls),
+    path("", welcome),
+]
